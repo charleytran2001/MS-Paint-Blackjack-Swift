3f642c2e0d0b72b65395afc2e1289efaa9d7c776
charleytran2001 <charleytran2001@gmail.com>
2024-02-09 16:11:52 -0700

    Added Basic Functionality
    Added in card deck generator, card drawing, and card total.

---
+++ MS Paint Blackjack.swiftpm/CardDeck.swift
@@ -0,0 +1,51 @@
+//
+//  CardDeck.swift
+//  MS Paint Blackjack
+//
+//  Created by Charley Tran on 2/5/24.
+//
+
+import Foundation
+
+struct Card {
+    let suit: String
+    let face: String
+    
+    init(suit: String, face: String) {
+        self.suit = suit
+        self.face = face
+    }
+}
+
+class Deck {
+    var deckArray: [Card] = []
+
+    let suitArray = ["S", "H", "C", "D"]
+    let faceArray = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    
+    var totalCards = 0
+    var drawnCards = 0
+    
+    init(numOfDecks: Int) {
+        for _ in 1...numOfDecks {
+            for suit in suitArray {
+                for face in faceArray {
+                    let card = Card(suit: suit, face: face)
+                    deckArray.append(card)
+                    totalCards += 1
+                }
+            }
+        }
+        deckArray.shuffle()
+    }
+    
+    func drawCard() -> Card {
+        drawnCards += 1
+        return deckArray.last!
+    }
+    
+    func removeLastCard() {
+        totalCards -= 1
+        deckArray.removeLast()
+    }
+}
